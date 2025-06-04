6801ba89b2751bc5e4b42e7d8eddab5ae5932d0a
Lethabo Kgopa <lkgopa@chieta.org.za>
2025-06-04 10:40:50 +0200

    Landing Page UI final

--- ChietaApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9b2d7918e16e729260021753352b382f925a09ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+258192526efb0e07b38f0504f06f31731bc001cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
