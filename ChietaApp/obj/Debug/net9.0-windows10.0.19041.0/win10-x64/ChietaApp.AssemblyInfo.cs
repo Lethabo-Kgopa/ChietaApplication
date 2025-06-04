ab1f990800b55a6d64360dd95e81385f0105f0fe
Lethabo Kgopa <lkgopa@chieta.org.za>
2025-06-04 10:56:16 +0200

    Merge developing branch to LK_dev

--- ChietaApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ChietaApp.AssemblyInfo.cs
@@ -11,14 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "com.companyname.chietaapp")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "ChietaApp")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+258192526efb0e07b38f0504f06f31731bc001cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0fc5fc78f0b90f04a39151bb8586e81065c25e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
