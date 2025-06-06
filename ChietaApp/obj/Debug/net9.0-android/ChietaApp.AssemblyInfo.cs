7818e57cce0d50e94a67c32575f3d3fdee8b53d1
Nokubonga Thabede <nthabede@chieta.org.za>
2025-06-06 16:56:11 +0200

    View Linked organization

--- ChietaApp/obj/Debug/net9.0-android/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-android/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1bcad00dba7d75077439cd4e29987fd8ec50921b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5a0fc5fc78f0b90f04a39151bb8586e81065c25e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ChietaApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-maccatalyst/maccatalyst-x64/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+258192526efb0e07b38f0504f06f31731bc001cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5a0fc5fc78f0b90f04a39151bb8586e81065c25e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
