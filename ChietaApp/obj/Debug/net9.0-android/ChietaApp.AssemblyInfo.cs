98e7e4691ab46efd0e71b0bb2e210f5efa74580c
Nokubonga Thabede <nthabede@chieta.org.za>
2025-06-08 19:41:35 +0200

    merging

--- ChietaApp/obj/Debug/net9.0-android/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-android/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da60c07d0d719bc4df17b4426a8b36c646a66e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6073932dde325c96671e5bbb7b7c43f5e95b7a87")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
