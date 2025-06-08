99aed0453ec0b2c95819183e038790b4965f9f53
Lethabo Kgopa <lkgopa@chieta.org.za>
2025-06-08 19:21:55 +0200

    Additions before merge

--- ChietaApp/obj/Debug/net9.0-ios/ios-arm64/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-ios/ios-arm64/ChietaApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ccbbcfabff4bd26d4b936c88c41f97d1e32ee248")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9f3c801e8f238a2239ee1d3652a062e4ca454c8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
