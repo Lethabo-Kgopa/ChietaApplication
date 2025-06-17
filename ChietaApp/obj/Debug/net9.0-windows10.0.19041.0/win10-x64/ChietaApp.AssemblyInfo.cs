3d78732d2e267f081eadc932bb80846a71016048
Nokubonga Thabede <nthabede@chieta.org.za>
2025-06-17 10:37:30 +0200

    changes before pull

--- ChietaApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ChietaApp.AssemblyInfo.cs
+++ ChietaApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/ChietaApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fa1d6d3ab086acc2a1b417775d7e7a492f01b8a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d07512e80572cc220cc4769763183edf96f8df09")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChietaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
